f4536511014bda107dcb573ae9e7cf91c1820445
Javaria Brascom <javariab17@gmail.com>
2020-10-12 15:58:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+name_hash.each do |name ,number|
 
+if number < 100
+  winner= name 
+  
+  number+=1
+
+end
 end
