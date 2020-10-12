b3cdbaf404646a2e8f6c46b9dd64e899f66e6853
Javaria Brascom <javariab17@gmail.com>
2020-10-12 16:48:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 name_hash.each do |key, value|
 
 if min_value == nil || value < min_value
- min_key= key 
+ min_key = key 
 min_value = value
 else if value < min_value
   min_key = key
